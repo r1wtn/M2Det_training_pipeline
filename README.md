cf96a95bfb309fd454a33a3786e8ab55305f0430
fltwtn <tomoya.watanabe@firstloop-tech.com>
2019-09-10 20:37:59 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,126 @@
 # M2Det_training_pipeline
-tools to create original M2det training dataset
+## はじめに
+オブジェクト検知で高いパフォーマンスを発揮するM2Detですが、オリジナルデータを使って学習させた例が少ないため、必要なワークフローをツールとともにここにまとめます。  
+M2Detの実装は、 https://github.com/qijiezhao/M2Det をクローンしてビルドしてください。
+## 環境
+ローカルPC
+* Mac OS Mojave 10.14.16
+* python 3.6.8
+学習用サーバ
+* CentOS
+* CUDA V10.1.243
+* cuDNN 7.6.3
+* GeForce GTX 1080
+## ワークフロー概観
+1. データパッケージの構成
+2. Vatic annotation toolによるアノテーション
+3. データの水増しと必要ファイルの生成
+4. サーバへのデータ転送
+5. M2Det設定ファイルの変更
+6. 学習スタート
+## データパッケージの構成
+データセットをローカルPCで作成し、zipファイルにまとめてサーバに送る運用を想定しています。  
+このプロジェクト内では以下のようなディレクトリ構成にします。
+```
+root/
+    ├ data/
+    │
+    ├ vatic_annotation/
+    │    └ <data_name>/
+    │         └ videos_in/
+    │              └ <target_video>
+    │
+    └ generate_dataset.py
+```
+## Vatic annotation tool によるアノテーション
+### インストール
+以下の記事にしたがってDockerによる環境構築を行ってください。  
+https://qiita.com/wakaba130/items/b0db5c5af4d1ecdaf985  
+2019年9月10日現在、以下のスクリプトにミスがあるので、vimで修正してください。  
+```
+# cd root/vatic/
+# vi cli.py
+
+Line No.933, 934
+file.write("<height>{0}</height>".format(video.width))
+file.write("<width>{0}</width>".format(video.height))
+>>
+file.write("<height>{0}</height>".format(video.height))
+file.write("<width>{0}</width>".format(video.width))
+```
+### アノテーション実行
+rootティレクトリ直下で、以下のコマンドを実行してコンテナに入ります。
+```
+$ docker run -it -p 8111:80 -v $PWD/vatic_annotation/<data_name>:/root/vatic/data npsvisionlab/vatic-docker /bin/bash
+```
+上記のスクリプトミスを修正したのち、Qiita記事にしたがって実行スクリプトを走らせ、ブラウザで http://localhost:8111/directory/ に入ります。
+![vatic](./example/vatic.png)
+画像の矢印で示した部分に、occlusion（対象が別のオブジェクトによって遮蔽されること）の有無を設定できるボタンがあります。  
+事前に決定したルール（例えば対象の３割以上が遮蔽されていたらアノテーションしない）にしたがって、occlusionの有無を切り替えてください。  
+occlusionをONにしている時にはバウンディングボックスが点線になりますが、追跡は継続されます。  
+ここでOcclusionフラグを付与されたデータは、後続の処理によってデータセットから排除されます。
+## データの水増しと必要ファイルの生成
+root直下で以下を実行
+```
+$ python generate_dataset.py -i vatic_annotation/<data_name> -x 30 -y 30
+```
+すると、data以下が以下のような構成になります。
+```
+data/
+    └ VOC<data_name>/
+         ├ Annotations/
+         │     ├ xxx0.xml
+         │     ├ xxx1.xml
+         │
+         ├ ImageSets/
+         │     ├ xxx0.jpg
+         │     ├ xxx1.jpg
+         │
+         └ ImageSets/
+               └ Main
+                  ├ test.txt
+                  └ trainval.txt
+
+```
+generate_dataset.py では、オプションで指定したpixel数だけ画像とラベルを上下左右に移動させることでデータを水増しし、同時にランダムにシャッフルして訓練データと検証データを生成します。
+オプションはコードを参照してください。以下のレポジトリを参考に作成しています。  
+https://github.com/ssaru/convert2Yolo
+## サーバへのデータ転送
+```
+VOC<data_name>
+```
+をZipで圧縮して、サーバに転送します。FileZillaを使うと便利です。  
+https://filezilla-project.org/  
+サーバのM2Detプロジェクトは、ビルドしてVOCデータのダウンロードが終わっていれば以下のような構成になっているはずです。
+```
+root/
+    ├ configs/
+    │     ├ m2det512_vgg.py
+    │
+    ├ data/
+    │     ├ VOCdevkit
+    │
+    │
+    ├ train.py
+```
+圧縮したzipファイルは、data/VOCdevkit直下に転送して解凍します。
+## M2Det設定ファイルの変更
+m2det512_vgg.pyを修正します。モデルに関する基本的な設定は全てこのファイルに集約されているため、エポック数や学習率の設定変更もこのファイルをいじります。  
+今回は自分のデータを指定するところまで設定します。
+```python
+# 64行目
+# train_sets = [('2007', 'trainval'), ('2012', 'trainval')],
+train_sets = [('<data_name>', 'trainval')],
+```
+## 学習スタート
+```
+CUDA_VISIBLE_DEVICES=2,3 screen python train.py -c=configs/m2det512_vgg.py --ngpu 2 -d VOC
+```
+GPUは使いたい番号を指定してください。  
+screenコマンドはプロセスをバックグランドで回すために利用します。これをしておかないとサーバとのsshセッションが切れた時に学習がストップします。  
+これで学習がスタートするはずです。お疲れ様でした。
+## Tips
+M2Detの学習は鬼のように時間がかかるため、放置して帰宅することが多いです。とはいえプロセスが生きているか心配なので、筆者はSlack botをtrain.pyに仕込んでいます。  
+https://qiita.com/vmmhypervisor/items/18c99624a84df8b31008  
+https://qiita.com/shtnkgm/items/4f0e4dcbb9eb52fdf316  
+以上のサイトを参考に、train.pyにslackwebを導入します。save_weightsが終了したくらいのタイミングでメッセージ（例えば現在のエポック数）をPOSTするようにしておけば、Slackでプロセスの死活監視が可能になります。
---
+++ generate_dataset.py
@@ -0,0 +1,151 @@
+import numpy as np
+import random
+from PIL import Image
+import glob
+import random
+import argparse
+from tqdm import tqdm
+import os
+import xml.etree.ElementTree as ET
+
+def create_dataset_config(data_name, class_num, ratio):
+    if ratio is None:
+        raise TypeError("Please indicate ratio arg")
+    img_dir = 'data/VOC' + data_name + "/JPEGImages/*.jpg"
+    #label_dir = data_name + "/labels/*"
+    images = sorted(glob.glob(img_dir))
+    random.shuffle(images)
+    
+    #labels = sorted(glob.glob(label_dir))
+
+    train_len = int(len(images)*ratio)
+    with open('data/VOC' + data_name + '/ImageSets/Main/' + '/trainval.txt', 'w') as f:
+        for text in images[:train_len]:
+            text = text.split('/')[-1]
+            text = text.split('.')[0]
+            f.write(text + "\n")
+
+    with open('data/VOC' + data_name + '/ImageSets/Main/' + '/test.txt', 'w') as f:
+        for text in images[train_len:]:
+            text = text.split('/')[-1]
+            text = text.split('.')[0]
+            f.write('data/' + text + "\n")
+
+
+def preprocessVoc(img_path, xml_path, offset):
+    """
+    shift the position of bounding boxes on both images and labels
+    
+    Parameters
+    ----------
+    img_path : path to image file
+    label : path to label file
+    offset : tuple(x, y) to shift
+    out_name : ex) 
+    
+    Returns:
+    img : shifted image 
+    """
+    img = np.asarray(Image.open(img_path).convert('RGB'))
+
+    h, w, _ = img.shape
+    
+    xmlRoot = ET.parse(xml_path).getroot()
+    xmlRoot.find('filename').text = img_path.split('/')[-1]
+
+    for member in xmlRoot.findall('object'):
+        bndbox = member.find('bndbox')
+
+        xmin = bndbox.find('xmin')
+        ymin = bndbox.find('ymin')
+        xmax = bndbox.find('xmax')
+        ymax = bndbox.find('ymax')
+
+        # shift label boxes
+        x1_ = float(xmin.text) + offset[0]
+        y1_ = float(ymin.text) + offset[1]
+        x2_ = float(xmax.text) + offset[0]
+        y2_ = float(ymax.text) + offset[1]
+        # box conditions
+        cond_x1 = x1_ <= 0
+        cond_y1 = y1_ <= 0
+        cond_x2 = x2_ >= w
+        cond_y2 = y2_ >= h
+
+        xmin.text = str(np.round(int(x1_)))
+        ymin.text = str(np.round(int(y1_)))
+        xmax.text = str(np.round(int(x2_)))
+        ymax.text = str(np.round(int(y2_)))
+
+        # judge wheter object is in bad conditions
+        occluded = int(member.find('occluded').text)
+        invalid = cond_x1+cond_y1+cond_x2+cond_y2+occluded
+
+        if invalid:
+            xmlRoot.remove(member)
+
+    tree = ET.ElementTree(xmlRoot)
+
+
+    # shift image
+    pad_x = np.zeros((h,abs(offset[0]),3))
+    pad_y = np.zeros((abs(offset[1]),w,3))
+    if offset[0] > 0:
+        img = np.concatenate([pad_x, img], axis=1)
+        img = np.delete(img, slice(w, None), axis=1)
+        img = img.astype('uint8')
+    else:
+        img = np.concatenate([img, pad_x], axis=1)
+        img = np.delete(img, slice(None, abs(offset[0])), axis=1)
+        img = img.astype('uint8')
+    if offset[1] > 0:
+        img = np.concatenate([pad_y, img], axis=0)
+        img = np.delete(img, slice(h, None), axis=0)
+        img = img.astype('uint8')
+    else:
+        img = np.concatenate([img, pad_y], axis=0)
+        img = np.delete(img, slice(None, abs(offset[1])), axis=0)
+        img = img.astype('uint8')
+
+    return img, tree
+
+
+parser = argparse.ArgumentParser(description='shift image')
+parser.add_argument('-i', '--input', help='path to root directory of input')
+parser.add_argument('-x', '--diff_x', type=int, help='pixel to shift on x axis')
+parser.add_argument('-y', '--diff_y', type=int, help='pixel to shift on y axis')
+parser.add_argument('-m', '--magnification', type=int, default=5, help='magnification of images')
+parser.add_argument('-c', '--class_num', type=int, default=1, help='number of classes')
+parser.add_argument('-r', '--ratio', type=float, default=0.7, help='number of classes')
+args = parser.parse_args()
+
+diff_x = args.diff_x
+diff_y = args.diff_y
+magnification = args.magnification
+class_num = args.class_num
+data_name = args.input.split('/')[-1]
+ratio = args.ratio
+
+img_out = 'data/VOC' + data_name + '/JPEGImages'
+xml_out = 'data/VOC' + data_name + '/Annotations'
+dataset_out = 'data/VOC' + data_name + '/ImageSets/Main'
+images_path = sorted(glob.glob(args.input + '/VOC/JPEGImages/*.jpg'))
+xmls_path = sorted(glob.glob(args.input + '/VOC/Annotations/*.xml'))
+
+
+if os.path.exists(img_out):
+    pass
+else:
+    os.makedirs(img_out)
+    os.makedirs(xml_out)
+    os.makedirs(dataset_out)
+
+offset_list = [tuple((random.randint(-diff_x,diff_x),random.randint(-diff_y,diff_y))) for i in range(magnification)]
+
+for i, (image_path, xml_path) in tqdm(enumerate(zip(images_path, xmls_path))):
+    for j, offset in enumerate(offset_list):
+        n_img, n_label = preprocessVoc(image_path, xml_path, offset)
+        Image.fromarray(n_img).save(img_out+f'/{i:04d}_{j:03d}.jpg')
+        n_label.write(xml_out+f'/{i:04d}_{j:03d}.xml')
+
+create_dataset_config(data_name, class_num, ratio)
